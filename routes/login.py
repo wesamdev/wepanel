70f7553c61871f154fbca3c0fcf2ed3c96842c85
wesam <software.market999@gmail.com>
2024-04-03 18:15:48 +0400

    Update login.py

--- routes/login.py
+++ routes/login.py
@@ -1,6 +1,11 @@
 from flask import render_template
 from flask import redirect, url_for, session
 from authlib.integrations.flask_client import OAuth
+import json
+
+with open('config.json') as f:
+    config = json.load(f)
+
 
 def login():
     return render_template('login.html')
@@ -12,14 +17,15 @@ def init_outh(app):
     oauth = OAuth(app)
     discord = oauth.register(
         name='discord',
-        client_id='your_discord_client_id',
-        client_secret='your_discord_client_secret',
+        client_id=config['discord']['client_id'],
+        client_secret=config['discord']['client_secret'],
         authorize_url='https://discord.com/api/oauth2/authorize',
         authorize_params=None,
         access_token_url='https://discord.com/api/oauth2/token',
         access_token_params=None,
         refresh_token_url=None,
         client_kwargs={'scope': 'identify'}
+        
     )
 
 
