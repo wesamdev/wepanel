f62e10f24316b081594d598aff68142e1099b853
wesam <software.market999@gmail.com>
2024-04-04 14:02:28 +0400

    :construction: :bug:
    fix coins in userinfo page

--- routes/userinfo.py
+++ routes/userinfo.py
@@ -1,4 +1,10 @@
 from flask import render_template, session
+import requests
+import json
+
+with open('config.json') as f:
+    config = json.load(f)
+
 
 def userinfo():
     username = session.get('username')
@@ -6,6 +12,10 @@ def userinfo():
     userid = session.get('userid')
 
     if username and avatarurl and userid:
-        return render_template('userinfo.html', user_name=username, user_id=userid, avatar_url=avatarurl)
+        usercoins = requests.get(f"{config['panel']['url']}/api/user/{userid}" + "/coins", headers={"Authorization": config['panel']['apikey']})
+        if usercoins.status_code == 200:
+            coins_data = usercoins.json()
+            coins = coins_data.get("coins")
+            return render_template('userinfo.html', user_name=username, user_id=userid, avatar_url=avatarurl,user_coins = coins)
     else:
         return 'User information not available'
