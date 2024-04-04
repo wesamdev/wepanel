ab9e9211c489d9af33de24a6ce98ba14943b8fd2
wesam <software.market999@gmail.com>
2024-04-04 13:42:59 +0400

    :bug: Make it json

--- api/db.py
+++ api/db.py
@@ -1,15 +1,16 @@
 # db.py
-from flask import request, session, redirect, url_for
+from flask import jsonify, request, redirect, url_for
 from pymongo import MongoClient
 client = MongoClient("mongodb://localhost:27017/")
 db = client["wepanel"] 
 users_collection = db["users"]
 
 
+# make this func json def get_user_coins(userid):
+
 def get_user_coins(userid):
     if request.method == "POST":
-        userid = request.form.get("userid")
         user = users_collection.find_one({"userid": userid})
-        return user["coins"]
+        return jsonify({"coins": user["coins"]})
     else:
         return redirect(url_for("login"))
--- routes/dashboard.py
+++ routes/dashboard.py
@@ -1,11 +1,23 @@
 from flask import render_template, session
+import requests
+import json
 
+with open('config.json') as f:
+    config = json.load(f)
 def dashboard():
     username = session.get('username')
     avatarurl = session.get('avatarurl')
     userid = session.get('userid')
-
-    if username and avatarurl and userid:
-        return render_template('dashboard.html', user_name=username, user_id=userid, avatar_url=avatarurl)
+    print("Requesting")
+    # use jsonfiy to get coins
+    usercoins = requests.post(f"{config['panel']['url']}/api/user/{userid}" + "/coins", headers={"Authorization": config['panel']['apikey']})
+    if usercoins.status_code == 200:
+        coins_data = usercoins.json()
+        coins = coins_data.get("coins")
+        print(username, avatarurl, userid, coins)
+        if username and avatarurl and userid and coins:
+            return render_template('dashboard.html', user_name=username, user_id=userid, avatar_url=avatarurl,user_coins = coins)
+        else:
+            return 'User information not available'
     else:
         return 'User information not available'
