6fb28ed98334e09c47f09293bb7da50bfd04d2b6
wesam <software.market999@gmail.com>
2024-04-04 13:59:23 +0400

    :zap:
    Make it return to login if it not logged in

--- routes/dashboard.py
+++ routes/dashboard.py
@@ -1,20 +1,24 @@
-from flask import render_template, session
+from flask import render_template, session, redirect, url_for
 import requests
 import json
 
 with open('config.json') as f:
     config = json.load(f)
 def dashboard():
+
     username = session.get('username')
     avatarurl = session.get('avatarurl')
     userid = session.get('userid')
-    print("Requesting")
-    # use jsonfiy to get coins
-    usercoins = requests.get(f"{config['panel']['url']}/api/user/{userid}" + "/coins", headers={"Authorization": config['panel']['apikey']})
-    if usercoins.status_code == 200:
-        coins_data = usercoins.json()
-        coins = coins_data.get("coins")
-        print(username, avatarurl, userid, coins)
-        return render_template('dashboard.html', user_name=username, user_id=userid, avatar_url=avatarurl,user_coins = coins)
+    if username and avatarurl and userid:
+        print("Requesting")
+        # use jsonfiy to get coins
+        usercoins = requests.get(f"{config['panel']['url']}/api/user/{userid}" + "/coins", headers={"Authorization": config['panel']['apikey']})
+        if usercoins.status_code == 200:
+            coins_data = usercoins.json()
+            coins = coins_data.get("coins")
+            print(username, avatarurl, userid, coins)
+            return render_template('dashboard.html', user_name=username, user_id=userid, avatar_url=avatarurl,user_coins = coins)
+        else:
+            return 'User information not available'
     else:
-        return 'User information not available'
+        return redirect(url_for('login'))
